a7cd6b87a3f7241e48b15712259ab0a65dc61117
matan515 <diz.matan@gmail.com>
2025-12-15 10:22:55 +0200

    i add code wgat we need to get

--- lib/widgets/nt_widgets/multi-topic/limeLitte.dart
+++ lib/widgets/nt_widgets/multi-topic/limeLitte.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:elastic_dashboard/services/nt4_client.dart'
+import 'package:elastic_dashboard/widgets/nt_widget.dart';
 
-class limeLotte extends MultiTopicNTWidgetModel{
+class limeLotte extends NTWidget {
+
+  NT4Topic 
+
+  private Color backGroubdndColor;
+  private Color textColor;
+  private String text;
+
+  limeLotte({
+    super.key,
+    required super.topic,
+    super.dataType,
+    super.period,
+    required this.backGroubdndColor = Colors.Perple,
+    required this.textColor = Colors.blue,
+    required this.text,
+  })  
 
 }
 
