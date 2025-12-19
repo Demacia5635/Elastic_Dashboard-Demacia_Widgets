febb4dc9686de1f4807130e688831065fb804367
demacia desktop <demacia1212@gmail.com>
2025-12-19 16:18:11 +0200

    the start

---
+++ lib/widgets/nt_widgets/multi-topic/limelight.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:dot_cast/dot_cast.dart';
+import 'package:provider/provider.dart';
+
+import 'package:elastic_dashboard/services/nt4_client.dart';
+import 'package:elastic_dashboard/widgets/dialog_widgets/dialog_toggle_switch.dart';
+import 'package:elastic_dashboard/widgets/nt_widgets/nt_widget.dart';
+
+class Limelight extends MultiTopicNTWidgetModel {
+  @override
+  String type = Limelight.widgetType
+
+  double get txTopicName => '$topic/tx';
+  double get tyTopicName => '$topic/ty';
+  String get pipelineTopicName => '$topic/pipeline';
+  String get sensor_grainTopicName => '$topic/sensor_grain';
+  String get black_level_offsetTopicName => '$topic/black_level_offset';
+  String get exposureTopicName => '$topic/expusure';
+
+  late NT4Subscription txSubscription;
+  late NT4Subscription tySubscription;
+  late NT4Subscription pipelineSubscription;
+  late NT4Subscription sensor_grainSubscription;
+  late NT4Subscription black_level_offsetSubscription;
+  late NT4Subscription expusureSubscription;
+  @override
+    List<NT4Subscription> get subscriptions => [
+        txSubscription,
+        tySubscription,
+        pipelineSubscription,
+        sensor_grainSubscription,
+        black_level_offsetSubscription,
+        expusureSubscription,
+      ];
+      Limelight({
+    required super.ntConnection,
+    required super.preferences,
+    required super.topic,
+    super.dataType,
+    super.period,
+  });
+
+
+
+}
